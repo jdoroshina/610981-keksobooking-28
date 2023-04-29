4c143bb732faaa2a397a1bf2883b4be7f49d3e1d
julia doroshina <yuspiri@gmail.com>
2023-04-29 13:28:12 +0200

    Create util functions

--- js/utils.js
+++ js/utils.js
@@ -7,13 +7,22 @@ const getRandomInt = (minRandomValue, maxRandomValue) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
 
-const createIdGenerator = () => {
-  let lastGeneratedId = 0;
-
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
+const getRandomArrValues = (arr, valuesQuantity) => {
+  const result = arr.slice(); // Создаем копию исходного arr
+  result.sort(() => Math.random() - 0.5); // Сортируем массив случайным образом
+  return result.slice(0, valuesQuantity); // Возвращаем первые valuesQuantity элементов
 };
 
-export { getRandomInt, getRandomArrayElement, createIdGenerator };
+const getRandomUniqueArrValues = (arr, valuesQuantity) => {
+  const result = arr.slice(); // Создаем копию исходного arr
+  result.sort(() => Math.random() - 0.5); // Сортируем массив случайным образом
+  const uniqueValues = [...new Set(result.slice(0, valuesQuantity))]; // Удаляем повторяющиеся значения
+  return uniqueValues;
+};
+
+const generateRandomFloat = (minValue, maxValue, decimalPlaces) => {
+  const randomValue = Math.random() * (maxValue - minValue) + minValue;
+  return randomValue.toFixed(decimalPlaces);
+};
+
+export { getRandomInt, getRandomArrayElement, getRandomArrValues, getRandomUniqueArrValues, generateRandomFloat };
