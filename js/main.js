feac826574f3f958ce6ff6de0a5d26ba68959793
julia doroshina <yuspiri@gmail.com>
2023-05-03 12:11:31 +0200

    fixup! Export switcher functions to main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { getAds } from './data.js';
 import { renderPopups } from './popup.js';
-import { switchOfferFormOff, switchOfferFormOn, switchFilterFormOff, switchFilterFormOn } from 'offer-form.js';
+import { switchOfferFormOff, switchOfferFormOn, switchFilterFormOff, switchFilterFormOn } from './offer-form.js';
 
 renderPopups(getAds(1));
 
