df39a538cabafc2d59ff77074c972f2bc0a61caa
julia doroshina <yuspiri@gmail.com>
2023-05-11 11:01:06 +0200

    Set pristine

--- js/offer-form.js
+++ js/offer-form.js
@@ -3,6 +3,16 @@ const offerFormFieldsets = offerForm.querySelectorAll('fieldset');
 const offerFormSlider = offerForm.querySelector('.ad-form__slider');
 const filterForm = document.querySelector('.map__filters');
 const filterFormFieldsets = filterForm.querySelectorAll('fieldset');
+const pristine = new Pristine(offerForm,
+  {
+    classTo: 'ad-form__element',
+    errorTextParent: 'ad-form__element',
+    errorClass: 'ad-form__element--invalid',
+    errorTextTag: 'span',
+    errorTextClass: 'text-help',
+  },
+  true
+);
 
 const switchOfferFormOff = () => {
   offerForm.classList.add('ad-form--disabled');
