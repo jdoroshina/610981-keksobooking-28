4d05d662a8eb1c8ab2772d885917dcf4b1934aa2
julia doroshina <yuspiri@gmail.com>
2023-05-12 16:16:54 +0200

    Add map.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { getAds } from './data.js';
 import { renderPopups } from './popup.js';
 import { switchOfferFormOff, switchOfferFormOn, switchFilterFormOff, switchFilterFormOn } from './offer-form.js';
+import './map.js';
 
 renderPopups(getAds(1));
 
