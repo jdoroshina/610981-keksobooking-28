7964e2b20eae1be52c2ce3cfadd4da6d01959af8
julia doroshina <yuspiri@gmail.com>
2023-05-27 16:04:47 +0200

    Add success and error messages

--- js/show-message.js
+++ js/show-message.js
@@ -1,4 +1,50 @@
+import { isEscKey } from './utils.js';
+
 const ALERT_TIMEOUT = 2000;
+
+const bodyElement = document.body;
+
+const successMessageTemplate = bodyElement.querySelector('#success')
+  .content
+  .querySelector('.success');
+
+const errorMessageTemplate = bodyElement.querySelector('#error')
+  .content
+  .querySelector('.error');
+
+const hideMessage = () => {
+  const messageElement = bodyElement.querySelector('.success') || bodyElement.querySelector('.error');
+  messageElement.remove();
+  bodyElement.style.overflow = 'auto';
+};
+
+const onMouseClick = () => hideMessage();
+
+const onEscKeydown = (evt) => {
+  if (isEscKey(evt)) {
+    evt.preventDefault();
+    hideMessage();
+    document.removeEventListener('keydown', onEscKeydown);
+  }
+};
+
+const showSuccessMessage = () => {
+  const successMessageElement = successMessageTemplate.cloneNode(true);
+  successMessageElement.addEventListener('click', onMouseClick);
+  document.addEventListener('keydown', onEscKeydown);
+  bodyElement.append(successMessageElement);
+  bodyElement.style.overflow = 'hidden';
+};
+
+const showErrorMessage = () => {
+  const errorMessageElement = errorMessageTemplate.cloneNode(true);
+  const errorButton = errorMessageElement.querySelector('.error__button');
+  errorMessageElement.addEventListener('click', onMouseClick);
+  errorButton.addEventListener('click', onMouseClick);
+  document.addEventListener('keydown', onEscKeydown);
+  bodyElement.append(errorMessageElement);
+  bodyElement.style.overflow = 'hidden';
+};
 
 const showAlertMessage = (message) => {
   const alertElement = document.createElement('div');
@@ -12,4 +58,4 @@ const showAlertMessage = (message) => {
   }, ALERT_TIMEOUT);
 };
 
-export { showAlertMessage };
+export { showSuccessMessage, showErrorMessage, showAlertMessage };
