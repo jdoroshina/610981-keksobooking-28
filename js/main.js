7a9befe857a361a864f68945e7209d0898756254
julia doroshina <yuspiri@gmail.com>
2023-05-28 15:38:09 +0200

    Set submit and reset offer-form functions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-import { switchOfferFormOff, switchOfferFormOn, switchFilterFormOff, switchFilterFormOn } from './offer-form.js';
-import { initMap, setStartAddress, setOnMapLoad, setOfferPinMarker } from './map.js';
-import { showAlertMessage } from './show-message.js';
-import { getData } from './api.js';
+import { switchOfferFormOff, switchOfferFormOn, switchFilterFormOff, switchFilterFormOn, setOnOfferFormSubmit, setOnFormReset, setOnResetButton } from './offer-form.js';
+import { initMap, setStartAddress, setOnMapLoad, setOfferPinMarker, resetMap } from './map.js';
+import { showSuccessMessage, showErrorMessage, showAlertMessage } from './show-message.js';
+import { getData, sendData } from './api.js';
 
 const onGetDataSuccess = (offers) => {
   switchFilterFormOn();
   setOfferPinMarker(offers);
+  setOnResetButton(offers);
 };
 
 switchOfferFormOff();
 switchFilterFormOff();
 getData(onGetDataSuccess, showAlertMessage);
 initMap();
+
+const onSendDataSuccess = () => {
+  showSuccessMessage();
+  resetMap();
+  setOnFormReset();
+  setStartAddress();
+};
+
+setOnOfferFormSubmit(async (data) => {
+  await sendData(onSendDataSuccess, showErrorMessage, data);
+});
+
 setOnMapLoad();
 switchOfferFormOn();
 setStartAddress();
