c9953ef634d43d24dff11bbe81e0c6cefb48276c
julia doroshina <yuspiri@gmail.com>
2023-06-01 11:24:40 +0200

    Set width and height for ad-form__image

--- js/avatar.js
+++ js/avatar.js
@@ -1,7 +1,3 @@
-//1) Загрузка аватарки через поле загрузки файлов в блоке .ad-form__field
-// -надо навесить обработчик на нопку клик?
-// 2)Показать аватарку в блоке .ad-form-header__preview
-
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const DEFAULT_AVATAR = 'img/muffin-grey.svg';
 
